5e8accc51e1020b170f4ce90ffd8dbb03c5ef616
Kapish <mfkapish@gmail.com>
2025-11-05 15:06:27 +0530

    Initial commit - Potato Disease Classifier Project

---
+++ api_new.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI,UploadFile,File
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+
+app = FastAPI()
+
+endpoint = "http://localhost:8501/v1/models/my_model:predict"
+
+MODEL = tf.keras.models.load_model("C:/Users/KAPISH/PycharmProjects/potato_disease_classifier/models/1.keras")
+CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return {"message": "Hello!"}
+
+
+def read_file_as_image(data)-> np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+
+    return image
+
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    image_batch= np.expand_dims(image,0)  #model expects  a batch of images as input
+
+    json_data = {
+        "instances":image_batch.tolist()
+    }
+
+    response = requests.post(endpoint,json=json_data)
+    prediction =response.json()["predictions"][0]
+
+    predicted_class=CLASS_NAMES[np.argmax(prediction)]
+    confidence = np.max(prediction)
+
+    return {
+        "class": predicted_class,
+        "confidence": confidence
+    }
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
